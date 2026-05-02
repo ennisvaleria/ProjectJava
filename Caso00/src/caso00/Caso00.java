a55aadcfa27fb7a850467a02e9d31b3a9369ee8e
ennisvaleria <n00436429@upn.pe>
2026-05-02 00:10:22 -0500

    feat : cambios en Caso00

--- Caso00/src/caso00/Caso00.java
+++ Caso00/src/caso00/Caso00.java
@@ -16,6 +16,9 @@ public class Caso00 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Holaaaaxd");
+        
+        //prueba git stash y git develop
+        //holaaa
     }
     
 }
